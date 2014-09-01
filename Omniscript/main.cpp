e08f5ae64a7657bd8f6b16a2c12b5859026879e8
Aaron Houghton <aaronh@Aarons-MacBook-Pro.local>
2014-09-01 16:30:47 -0700

    Initial Commit

---
+++ Omniscript/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Omniscript
+//
+//  Created by Aaron Houghton on 9/1/14.
+//  Copyright (c) 2014 CCBAC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
